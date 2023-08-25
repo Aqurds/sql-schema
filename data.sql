fd471d296ec840414db0c7232ee106465cb5ff2f
aqurds <omarf1320@gmail.com>
2023-08-25 16:44:21 +0600

    Update table, query, roolback

--- data.sql
+++ data.sql
@@ -13,3 +13,15 @@ VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
 -- Insert Devimon's data
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon', '2017-05-12', 5, true, 11);
+
+
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES
+    ('Charmander', '2020-02-08', 0, false, -11.0, NULL),
+    ('Plantmon', '2021-11-15', 2, true, -5.7, NULL),
+    ('Squirtle', '1993-04-02', 3, false, -12.13, NULL),
+    ('Angemon', '2005-06-12', 1, true, -45.0, NULL),
+    ('Boarmon', '2005-06-07', 7, true, 20.4, NULL),
+    ('Blossom', '1998-10-13', 3, true, 17.0, NULL),
+    ('Ditto', '2022-05-14', 4, true, 22.0, NULL);
